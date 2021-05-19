d0c155c5a07b1ac3a31d0db646a06eb6fb3b67a6
TDanielyan4 <84374650+TDanielyan4@users.noreply.github.com>
2021-05-19 17:24:25 +0400

    Update Homework1.js

--- Homework1.js
+++ Homework1.js
@@ -1,8 +1,9 @@
 1st quiz
+
 let name = prompt('What is your name?','Tigran');
-undefined
+
 let surname = prompt('What is your surname?','Danielyan')
-undefined
+
 alert(`Your name is ${name} and surname is ${surname}`)
 __________________________________________________________________
 
